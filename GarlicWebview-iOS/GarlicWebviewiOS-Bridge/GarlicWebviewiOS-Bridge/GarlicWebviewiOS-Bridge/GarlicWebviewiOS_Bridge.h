19cdd701dd4f2d6697474415c4f5a5bc2fc4920d
TeamTapas <help@teamtapas.com>
2019-06-18 22:01:04 +0900

    Added iOS Unity-Bridge Project

---
+++ GarlicWebview-iOS/GarlicWebviewiOS-Bridge/GarlicWebviewiOS-Bridge/GarlicWebviewiOS-Bridge/GarlicWebviewiOS_Bridge.h
@@ -0,0 +1,19 @@
+//
+//  GarlicWebviewiOS_Bridge.h
+//  GarlicWebviewiOS-Bridge
+//
+//  Created by TeamTapas on 18/06/2019.
+//  Copyright © 2019 TeamTapas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GarlicWebviewiOS_Bridge.
+FOUNDATION_EXPORT double GarlicWebviewiOS_BridgeVersionNumber;
+
+//! Project version string for GarlicWebviewiOS_Bridge.
+FOUNDATION_EXPORT const unsigned char GarlicWebviewiOS_BridgeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GarlicWebviewiOS_Bridge/PublicHeader.h>
+
+
